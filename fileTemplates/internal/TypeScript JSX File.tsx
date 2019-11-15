269a79767f04863a4fc812512b7dcbd854d9e04f
DenneulinThomas <t.denneulin@apodispharma.com>
2019-11-15 14:37:16 +0100

    PS-2019.2.4 <denneulin@denneulin-XPS-13-9380 Update find.xml, filetypes.xml, vcs.xml, project.default.xml, colors.scheme.xml, databaseDrivers.xml, laf.xml, github.xml, Default.xml	Create keymap.xml, web-types-registry.xml, TypeScript JSX File.tsx, Default for GNOME copy.xml, editor.xml, updates.xml

---
+++ fileTemplates/internal/TypeScript JSX File.tsx
@@ -0,0 +1,21 @@
+import React, {FC, useEffect, useState} from 'react';
+import {StyleSheet, View} from "react-native"
+
+const ${NAME}: FC = (props) => {
+
+    useEffect(() => {
+        "Loader function"
+    },[]);
+
+    return (
+        <View>
+        OK
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ${NAME};
